cbf352a7b7ca31e475173d51e3dcd1b2562df69e
Bahtiyorov <okhunjon.bakhtiyori@gmail.com>
2022-01-05 09:54:08 +0500

    Scalable angular folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { FooterComponent } from './templates/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NotFoundComponent,
+    AuthComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
